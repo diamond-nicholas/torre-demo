6cc051ba6b1fa70585599103b3527c52df1d7eac
diamond-nicholas <diamondnicholas154@gmail.com>
2022-01-08 16:06:42 +0100

    add styles to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Menu from '../vectors/Menu';
 import SearchIcon from '../vectors/SearchIcon';
+import '../styles/Navbar.css';
 const Navbar = () => {
   return (
-    <div>
+    <div className='navbar'>
       <nav className='left'>
         <Menu />
         <p>
